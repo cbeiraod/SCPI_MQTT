66c9327c10544610086a8d5ecbbe789f7dc99de0
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2025-09-05 14:31:37 +0100

    More fixes for iseg quirks

--- iseg_SHR.py
+++ iseg_SHR.py
@@ -54,9 +54,13 @@ class ISEGSHR(Instrument):
         """Reset the instrument."""
         with self.mutex_lock:
             self.resource.query(":VOLT OFF,(@0-3)")
+            self.resource.read()
             self.resource.query("*CLS")
+            self.resource.read()
             self.resource.query("*RST")
+            self.resource.read()
             self.resource.query(":EVENT CLEAR,(@0-3)")
+            self.resource.read()
 
     def read(self) -> Dict[str, float]:
         """Reads the live output voltage and current of each channel."""
@@ -97,32 +101,42 @@ class ISEGSHR(Instrument):
         with self.mutex_lock:
             # Turn off the output and then reset the state
             self.resource.query(":VOLT OFF,(@0-3)")
+            self.resource.read()
             self.resource.query("*RST")
+            self.resource.read()
             self.resource.query("*CLS")
+            self.resource.read()
             self.resource.query(":EVENT CLEAR,(@0-3)")
+            self.resource.read()
 
             # Set Module Ramp % to 25%, emergency ramp to 200% and current ramp to 30%
             self.resource.query(f":CONF:RAMP:VOLT 25")
+            self.resource.read()
             self.resource.query(f":CONF:RAMP:VOLT:EMCY 200")
+            self.resource.read()
             self.resource.query(f":CONF:RAMP:CURR 30")
+            self.resource.read()
 
             # Retrieve averaging steps
             averaging_steps = supply_config.get("averaging_steps", "64")
             if averaging_steps not in ["1", "16", "64", "256", "512", "1024"]:
                 averaging_steps = "64"
             self.resource.query(f":CONF:AVER {averaging_steps}")
+            self.resource.read()
 
             # Retrieve kill enable
             kill_enable = supply_config.get("kill_enable", "0")
             if kill_enable not in ["0", "1"]:
                 kill_enable = "0"
             self.resource.query(f":CONF:KILL {kill_enable}")
+            self.resource.read()
 
             # Retrieve fine adjust
             fine_adjust = supply_config.get("fine_adjust", "1")
             if fine_adjust not in ["0", "1"]:
                 fine_adjust = "1"
             self.resource.query(f":CONF:ADJUST {fine_adjust}")
+            self.resource.read()
 
             # Loop on channels
             for channel in supply_config.get("channels"):
@@ -132,57 +146,71 @@ class ISEGSHR(Instrument):
                 trip_time_ms = int(float(channel_config.get("trip_time", "0.1"))*1000) # Trip time is defined in seconds
                 trip_action = find_SCPI(channel_config, 'trip_action', trip_action_SCPI, '4')
                 self.resource.query(f":CONF:TRIP:TIME {trip_time_ms},(@{channel_map[channel]})")
+                self.resource.read()
                 self.resource.query(f":CONF:TRIP:ACTION {trip_action},(@{channel_map[channel]})")
+                self.resource.read()
 
 
                 # Disable inhibit functionality
                 self.resource.query(f":CONF:INH:ACTION 4,(@{channel_map[channel]})")
+                self.resource.read()
 
                 # Retrieve output mode (see manual for details, valid modes are 1,2,3 depending on the specific model)
                 output_mode = channel_config.get("output_mode", "1") # TODO: Consider making this a text description... but the manual does not specify which number corresponds to which mode...
                 if output_mode not in ["1", "2", "3"]:
                     output_mode = "1"
                 self.resource.query(f":CONF:OUTPUT:MODE {output_mode},(@{channel_map[channel]})")
+                self.resource.read()
 
                 # Retrieve output polarity
                 output_polarity = channel_config.get("output_polarity", "n")
                 if output_polarity not in ["p", "n"]:
                     output_polarity = "n"
                 self.resource.query(f":CONF:OUTPUT:POL {output_polarity},(@{channel_map[channel]})")
+                self.resource.read()
 
                 # Retrieve ramp speeds
                 ramp_up   = int(channel_config.get("ramp_up", "250"))
                 ramp_down = int(channel_config.get("ramp_down", "500"))
                 self.resource.query(f":CONF:RAMP:VOLT:UP {ramp_up},(@{channel_map[channel]})")
+                self.resource.read()
                 self.resource.query(f":CONF:RAMP:VOLT:DOWN {ramp_down},(@{channel_map[channel]})")
+                self.resource.read()
 
                 curr_ramp_up   = float(channel_config.get("current_ramp_up", "2E-3"))
                 curr_ramp_down = float(channel_config.get("current_ramp_down", "4E-3"))
                 self.resource.query(f":CONF:RAMP:CURR:UP {curr_ramp_up},(@{channel_map[channel]})")
+                self.resource.read()
                 self.resource.query(f":CONF:RAMP:CURR:DOWN {curr_ramp_down},(@{channel_map[channel]})")
+                self.resource.read()
 
                 # Retrieve the current range
                 current_range = channel_config.get("current_range", "AUTO")
                 if current_range.upper() not in ["HIGH", "AUTO"]: # LOW is not yet supported
                     current_range = "AUTO"
                 self.resource.query(f":CONF:RANGE:CURR {current_range},(@{channel_map[channel]})")
+                self.resource.read()
 
                 # Get default power scheme
                 voltage = float(channel_config.get("voltage", 0))
                 current = float(channel_config.get("current", 0.0001))
                 self.resource.query(f":VOLT {voltage},(@{channel_map[channel]})")
+                self.resource.read()
                 self.resource.query(f":CURR {current},(@{channel_map[channel]})")
+                self.resource.read()
 
     def set_output(self, state: bool, channel: str = None) -> None:
         """Enable/disable output for one or all channels."""
         if channel is None:
             with self.mutex_lock:
                 self.resource.query(f":VOLT {'ON' if state else 'OFF'},(@0-3)")
+                self.resource.read()
         else:
             if channel not in channel_map:
                 raise ValueError(f"Channel {channel} must be a valid channel.")
             with self.mutex_lock:
                 self.resource.query(f":VOLT {'ON' if state else 'OFF'},(@{channel_map[channel]})")
+                self.resource.read()
 
     def set_voltage(self, voltage: float, channel: str = None) -> None:
         """Set voltage setpoint for a channel."""
